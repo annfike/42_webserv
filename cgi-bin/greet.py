3528db85d30c963a1a548fbd57caf4bbc82a079d
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2025-04-01 15:36:18 +0200

    test cgi files

---
+++ cgi-bin/greet.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import cgi
+
+print("Content-type: text/html\n")
+form = cgi.FieldStorage()
+greeting = form.getvalue("greeting")
+if greeting:
+    print(f"<h1>Your greeting: {greeting}</h1>")
+else:
+    print("<h1>No greeting provided.</h1>")
---
+++ cgi-bin/time.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import time
+
+print("Content-type: text/html\n")
+current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+print(f"<h1>Current time: {current_time}</h1>")
